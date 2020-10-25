0aeb7bd80bbf5eb0828f38ff000a1aeb6f35d99c
Tim-Seidell <46984533+Tim-Seidell@users.noreply.github.com>
2020-10-25 15:19:34 -0400

    Update settings.php

--- settings.php
+++ settings.php
@@ -5,7 +5,7 @@
         <div class = "row">
             <div class = "col-md text-center">
                 <?php
-                require 'includes/dbh.inc.php';
+                require 'includes/functions.inc.php';
                     $sql = "SELECT * FROM settings WHERE setting_name = 'editing'";
 
                     if(mysqli_fetch_assoc(mysqli_query($conn, $sql))['setting'] == 1) {
@@ -42,7 +42,7 @@
                         ';
                     }
 
-                    //setting_button("Test");
+                    settingButton("Test");
                 ?>  
 
             </div>
