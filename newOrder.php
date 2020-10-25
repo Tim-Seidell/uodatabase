ca953db3fb20951677d4da49d10f44279556466d
Tim-Seidell <46984533+Tim-Seidell@users.noreply.github.com>
2020-10-25 15:31:44 -0400

    descriptive Dropdowns

--- newOrder.php
+++ newOrder.php
@@ -37,13 +37,13 @@
                 ?>
                 <form action="" method = "post">
                     <select name="orderType" class = "dropdown" style = "width: 287px; height: 43px;">
-                        <?php echo '<option value="">'. 
-                                if(isset($_SESSION["orderType"])) {
-                                    echo $_SESSION["orderType"];
-                                } else {
-                                    echo '- Type -';
-                                }
-                                . '</option>';?>
+                        <?php 
+                            if(isset($_SESSION["orderType"])) {
+                                echo '<option value="">- Type -</option>';
+                            } else {
+                                echo '<option value="">- Custom -</option>';
+                            }
+                        ?>
                         <option value="">- Type -</option>
                         <option value="issue">Issue</option>
                         <option value="return">Return</option>
